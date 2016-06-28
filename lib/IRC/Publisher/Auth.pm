d62496ce40ac441b46ce4e53a6882a02f18cd848
Jon Portnoy <avenj@cobaltirc.org>
2016-06-28 16:12:55 -0400

    add simplistic Auth mgr
    Supports somewhat flexible policy combinations (whitelist, blacklist, passwd
    auth, in order specified via ->policy)

---
+++ lib/IRC/Publisher/Auth.pm
@@ -0,0 +1,161 @@
+package IRC::Publisher::Auth;
+
+use Carp;
+use strictures 2;
+
+use App::bmkpasswd 'passwdcmp';
+
+use List::Objects::WithUtils;
+use List::Objects::Types  -types;
+use Types::Standard       -types;
+
+use Moo;
+
+# TODO: pluggable auth backends
+
+has _valid_policies => (
+  is        => 'ro',
+  isa       => HashObj,
+  coerce    => 1,
+  builder   => sub {
+    +{ map {; $_ => 1 } qw/ -blacklist -whitelist -passwd / }
+  },
+);
+
+has policy => (
+  # Auth policy in force; ordered by priority
+  is        => 'ro',
+  isa       => ImmutableArray,
+  coerce    => 1,
+  writer    => 'set_policy',
+  builder   => sub {
+    [ -blacklist, -passwd ]
+  },
+  trigger   => 1,
+);
+
+sub _trigger_policy {
+  my ($self, $policy) = @_;
+  for my $chkpolicy (@$policy) {
+    confess "Auth policy '$chkpolicy' not a known policy"
+      unless $self->_valid_policies->exists($chkpolicy)
+  }
+}
+
+has accounts => (
+  # For -passwd auth policy; $account -> $passwd
+  is        => 'ro',
+  isa       => HashObj,
+  coerce    => 1,
+  builder   => sub { +{} },
+);
+
+has _addr_blacklist => (
+  is        => 'ro',
+  isa       => HashObj,
+  coerce    => 1,
+  builder   => sub { +{} },
+);
+
+sub get_blacklist { shift->_addr_blacklist->keys }
+
+sub blacklist {
+  my ($self, $addr) = @_;
+  confess "->blacklist expected an address" unless defined $addr;
+  $self->_addr_blacklist->set($addr => 1);
+  $self
+}
+
+sub unblacklist {
+  my ($self, $addr) = @_;
+  confess "->unblacklist expected an address" unless defined $addr;
+  unless ( $self->_addr_blacklist->exists($addr) ) {
+    carp "No blacklist entry found for addr '$addr'";
+    return
+  }
+  $self->_addr_blacklist->delete($addr);
+  $self
+}
+
+sub is_blacklisted {
+  my ($self, $addr) = @_;
+  confess "->is_blacklisted expected an address" unless defined $addr;
+  $self->_addr_blacklist->exists($addr)
+}
+
+has _addr_whitelist => (
+  is        => 'ro',
+  isa       => HashObj,
+  coerce    => 1,
+  builder   => sub { +{} },
+);
+
+sub get_whitelist { shift->_addr_whitelist->keys }
+
+sub whitelist {
+  my ($self, $addr) = @_;
+  confess "->whitelist expected an address" unless defined $addr;
+  $self->_addr_whitelist->set($addr => 1);
+  $self
+}
+
+sub unwhitelist {
+  my ($self, $addr) = @_;
+  confess "->unwhitelist expected an address" unless defined $addr;
+  $self->_addr_whitelist->delete($addr);
+  $self
+}
+
+sub is_whitelisted {
+  my ($self, $addr) = @_;
+  confess "->is_whitelisted expected an address" unless defined $addr;
+  $self->_addr_whitelist->exists($addr)
+}
+
+{ package
+    IRC::Publisher::AuthReturnValue;
+  sub new { bless [@_], shift }
+  sub allowed { shift->[0] }
+  sub message { shift->[1] }
+}
+
+sub _auth_retval {
+  IRC::Publisher::AuthReturnValue->new(@_)
+}
+
+sub check {
+  # Returns [$bool, $info_string] with ->allowed & ->message accessors
+  my ($self, $addr, @params) = @_;
+  
+  for my $chkpolicy ($self->policy->all) {
+    my $dispatch_to = substr $chkpolicy, 1;
+    my $meth = "_check_${dispatch_to}";
+    confess "BUG; no such method '$meth' for policy '$chkpolicy'"
+      unless $self->can($meth);
+    my $ret = $self->$meth($addr, @params);
+    $ret->[0] ? next POLICY : return _auth_retval(@$ret)
+  }
+
+  _auth_retval(1, 'allow')
+}
+
+sub _check_whitelist {
+  my ($self, $addr) = @_;
+  $self->is_whitelisted($addr) ? [1, 'allow'] : [0, 'not whitelisted']
+}
+
+sub _check_blacklist {
+  my ($self, $addr) = @_;
+  $self->is_blacklisted($addr) ? [0, 'blacklisted'] : [1, 'allow']
+}
+
+sub _check_passwd {
+  my ($self, undef, $acct, $crypted) = @_;
+  return [0, 'bad params'] unless defined $acct and $crypted;
+  return [0, 'unknown account'] unless $self->accounts->exists($acct);
+  my $passwd = $self->accounts->get($acct);
+  return [1, 'allow'] if passwdcmp $passwd => $crypted;
+  [0, 'bad passwd']
+}
+
+1;
